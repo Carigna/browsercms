e120f937d265dbf9a4b83aaa02416a9c9fd31da6
Toni Leino <toni@leino.net>
2009-04-09 15:03:05 +0300

    Localization changes

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -4,18 +4,18 @@ class ContentType < ActiveRecord::Base
   belongs_to :content_type_group
   validates_presence_of :content_type_group
   before_validation :set_content_type_group
-  
+
   named_scope :named, lambda{|name| {:conditions => ['content_types.name = ?', name]}}
-  
-  named_scope :connectable, 
+
+  named_scope :connectable,
     :include => :content_type_group,
     :conditions => ['content_type_groups.name != ?', 'Categorization'],
     :order => 'content_types.priority, content_types.name'
-  
+
   def self.list
     all.map { |f| f.name.underscore.to_sym }
   end
-   
+
   # Given a 'key' like 'html_blocks' or 'portlet'
   # Raises exception if nothing was found.
   def self.find_by_key(key)
@@ -34,17 +34,17 @@ class ContentType < ActiveRecord::Base
       content_type
     end
   rescue Exception
-    raise "Couldn't find ContentType of class '#{class_name}'"    
+    raise "Couldn't find ContentType of class '#{class_name}'"
   end
-  
+
   def is_child_of?(content_type)
-    name.constantize.ancestors.map{|c| c.name}.include?(content_type.name)  
+    name.constantize.ancestors.map{|c| c.name}.include?(content_type.name)
   end
-  
+
   def form
     model_class.respond_to?(:form) ? model_class.form : "cms/#{name.underscore.pluralize}/form"
   end
-  
+
   def display_name
     model_class.respond_to?(:display_name) ? model_class.display_name : model_class.to_s.titleize
   end
@@ -73,7 +73,7 @@ class ContentType < ActiveRecord::Base
   def content_block_type
     name.pluralize.underscore
   end
-  
+
   # This is used for situations where you want different to use a type for the list page
   # This is true for portlets, where you don't want to list all portlets of a given type,
   # You want to list all portlets
@@ -84,12 +84,12 @@ class ContentType < ActiveRecord::Base
       content_block_type
     end
   end
-  
+
   def set_content_type_group
     if group_name
       group = ContentTypeGroup.first(:conditions => {:name => group_name})
       self.content_type_group = group || build_content_type_group(:name => group_name)
     end
   end
-  
+
 end
